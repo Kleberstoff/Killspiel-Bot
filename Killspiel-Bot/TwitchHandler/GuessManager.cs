5fe39c6fc2c74b0671e44924a938b77c89604ed1
RoiEX <8350879+RoiEXLab@users.noreply.github.com>
2020-04-16 20:08:50 +0200

    Port JS code to C#

---
+++ Killspiel-Bot/TwitchHandler/GuessManager.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using TwitchLib.Client;
+
+namespace TwitchHandler
+{
+    class GuessManager
+    {
+        private readonly Dictionary<string, int> guesses = new Dictionary<string, int>();
+        private bool running = false;
+        private readonly TwitchClient twitchClient;
+
+        public GuessManager(TwitchClient twitchClient)
+        {
+            this.twitchClient = twitchClient;
+        }
+
+        public void Start(string channel, string user, string[] args) {
+            if (!running) {
+                guesses.Clear();
+            }
+            running = true;
+            Console.WriteLine("Awaiting guesses...");
+            twitchClient.SendMessage(channel, "Tipps werden jetzt angenommen!");
+        }
+
+        public void Stop(string channel, string user, string[] args) {
+            running = false;
+            Console.WriteLine("Guess window closed!");
+            twitchClient.SendMessage(channel, "Tipps werden nicht mehr angenmommen!");
+        }
+
+        public void Guess(string channel, string user, string[] args) {
+            if (args.Length == 1)
+            {
+                if (running)
+                {
+                    guesses[user] = int.Parse(args[0].Trim());
+                    Console.WriteLine($"User {user} guessed {guesses[user]}");
+                }
+                else
+                {
+                    twitchClient.SendMessage(channel, "Killspiel ist nicht aktiv!");
+                }
+            }
+            else
+            {
+                twitchClient.SendMessage(channel, "Ungültige Argumentzahl!");
+            }
+        }
+
+        public void Resolve(string channel, string user, string[] args) {
+            if (args.Length == 1 && !running)
+            {
+                if (!running)
+                {
+                    int realKills = int.Parse(args[0].Trim());
+                    List<string> winners = new List<string>();
+                    foreach (var guess in guesses)
+                    {
+                        if (guess.Value == realKills)
+                        {
+                            winners.Add(guess.Key);
+                        }
+                    }
+                    twitchClient.SendMessage(channel, "Gewonnen haben: " + string.Join(", ", winners));
+                }
+                else
+                {
+                    twitchClient.SendMessage(channel, "Killspiel läuft noch!");
+                }
+            }
+            else
+            {
+                twitchClient.SendMessage(channel, "Ungültige Argumentzahl!");
+            }
+        }
+
+    }
+}
--- Killspiel-Bot/TwitchHandler/Handler.cs
+++ Killspiel-Bot/TwitchHandler/Handler.cs
@@ -1,6 +1,94 @@
-﻿namespace TwitchHandler
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using TwitchLib.Client;
+using TwitchLib.Client.Enums;
+using TwitchLib.Client.Events;
+using TwitchLib.Client.Models;
+using TwitchLib.Communication.Clients;
+using TwitchLib.Communication.Models;
+
+namespace TwitchHandler
 {
 	public class Handler
-	{
-	}
+    {
+
+        const string command = "!killspiel ";
+
+        class CommandAction
+        {
+            public Predicate<UserType> Condition { get; set; }
+            public Action<string, string, string[]> Action { get; set; }
+        }
+
+        private readonly TwitchClient twitchClient;
+        private readonly GuessManager guessManager;
+        private readonly Dictionary<string, CommandAction> actions;
+
+        Handler(string username, string accessToken, string channel)
+        {
+            ConnectionCredentials credentials = new ConnectionCredentials(username, accessToken);
+            var clientOptions = new ClientOptions
+            {
+                MessagesAllowedInPeriod = 750,
+                ThrottlingPeriod = TimeSpan.FromSeconds(30)
+            };
+            WebSocketClient customClient = new WebSocketClient(clientOptions);
+            twitchClient = new TwitchClient(customClient);
+            twitchClient.Initialize(credentials, channel);
+            twitchClient.OnMessageReceived += Client_OnMessageReceived;
+
+            guessManager = new GuessManager(twitchClient);
+
+            actions = new Dictionary<string, CommandAction> {
+                ["start"] = new CommandAction { Condition = type => type >= UserType.Moderator, Action = guessManager.Start},
+                ["stop"] = new CommandAction { Condition = type => type >= UserType.Moderator, Action = guessManager.Stop },
+                ["tipp"] = new CommandAction { Condition = type => true, Action = guessManager.Guess },
+                ["kills"] = new CommandAction { Condition = type => type >= UserType.Moderator, Action = guessManager.Resolve },
+            };
+
+            twitchClient.Connect();
+        }
+
+        private void HandleCommand(string channel, string username, UserType userType, string command)
+        {
+            string[] args = command.Split(" ");
+            if (args.Length > 0)
+            {
+                CommandAction action;
+                if (actions.TryGetValue(args[0], out action))
+                {
+                    if (action.Condition(userType))
+                    {
+                        action.Action(channel, username, args.Skip(1).ToArray());
+                    }
+                    else
+                    {
+                        twitchClient.SendMessage(channel, "Keine Berechtigung!");
+                    }
+                }
+                else
+                {
+                    twitchClient.SendMessage(channel, "Ungültiger Befehl!");
+                }
+            }
+            else
+            {
+                twitchClient.SendMessage(channel, "Ungültiger Befehl!");
+            }
+        }
+
+        private void Client_OnMessageReceived(object sender, OnMessageReceivedArgs e)
+        {
+            var message = e.ChatMessage.Message;
+            var channel = e.ChatMessage.Channel;
+            var username = e.ChatMessage.Username;
+            var userType = e.ChatMessage.UserType;
+            if (message.StartsWith(command))
+            {
+                HandleCommand(channel, username, userType, message.Substring(command.Length));
+            }
+        }
+
+    }
 }
